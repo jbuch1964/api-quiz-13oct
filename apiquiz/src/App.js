ecc0be7a6937ca779627f5f3ca07c7a6a0574e1f
jbuchanon <jbuch1964@gmail.com>
2021-10-13 13:48:30 -0400

    NotSeeingAPIresult

--- apiquiz/src/App.js
+++ apiquiz/src/App.js
@@ -1,24 +1,66 @@
+import React, { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getList, setItem } from './list';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [alert, setAlert] = useState(false);
+  const [itemInput, setItemInput] = useState('');
+  const [list, setList] = useState([]);
+  const mounted = useRef(true);
+
+  useEffect(() => {
+    mounted.current = true;
+    if(list.length && !alert) {
+      return;
+    }
+    getList()
+      .then(items => {
+        if(mounted.current) {
+          setList(items)
+        }
+      })
+    return () => mounted.current = false;
+  }, [alert, list])
+ 
+  useEffect(() => {
+    if(alert) {
+      setTimeout(() => {
+        if(mounted.current) {
+        setAlert(false);
+        }
+      }, 1000)
+    }
+  }, [alert])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItem(itemInput)
+    .then(() => {
+      if(mounted.current) {
+      setItemInput('');
+      setAlert(true);
+      }
+    })
+  };
+
+
+  return ( 
+    <div className="wrapper">
+    <h1>Product List</h1>
+    <ul>
+      {list.map(item => <li key={item.item}>{item.item}</li>)}
+    </ul>
+    {alert && <h2> Submit Successful</h2>}
+    <form onSubmit={handleSubmit}>
+       <label>
+         <p>New Item</p>
+         <input type="text" onChange={event => setItemInput(event.target.value)} value={itemInput} />
+       </label>
+       <button type="submit">Submit</button>
+     </form>
+  </div>
   );
 }
 
